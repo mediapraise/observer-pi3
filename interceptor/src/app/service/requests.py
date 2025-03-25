d7529d94cd6bba39cf32e2f7c425dc94cc7221a7
Eduardo Rosa <edu.wd2014@gmail.com>
2025-03-25 00:01:02 -0300

    ♻️ refactor: refatoracao codigo

--- interceptor/src/app/service/requests.py
+++ interceptor/src/app/service/requests.py
@@ -8,16 +8,16 @@ import os
 from requests.auth import HTTPBasicAuth
 from settings import parameters
 
-# class CallApiGolang:
-#     ''' Classe para realizar chamadas de API para o Golang. '''
+class CallApiEndpoint:
+    ''' Classe para realizar chamadas de API para o Golang '''
 
-    # def post_request(url, data, headers=None):
+    def post_request(url, data, headers=None):
         
-    #     # Converte os dados em JSON
-    #     json_data = json.dumps(data)
+        # Converte os dados em JSON
+        json_data = json.dumps(data)
         
-    #     # Envia a requisição POST
-    #     response = requests.post(url, data=json_data, headers=headers)
+        # Envia a requisição POST
+        response = requests.post(url, data=json_data, headers=headers)
         
-    #     return response
+        return response
 
