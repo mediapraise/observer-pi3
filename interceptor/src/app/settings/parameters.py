8328d759d86e15de83c661b3a42231527038f6d5
Eduardo Rosa <edu.wd2014@gmail.com>
2025-03-25 00:01:36 -0300

    ♻️ refactor: refatoracao codigo

--- interceptor/src/app/settings/parameters.py
+++ interceptor/src/app/settings/parameters.py
@@ -1,6 +1,6 @@
 import enum
 
 class Parametros(enum.Enum):
-    ''' Classe de parâmetros para o interceptor. '''
+    ''' Classe de parâmetros para o interceptor '''
 
     url_api_golang = "http://localhost:8080/api/v1/interceptor"
