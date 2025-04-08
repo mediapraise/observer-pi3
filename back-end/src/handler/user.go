2951c67a9d23cf626f7bd7a05d6f6c7fdbdb1150
Luigi Azor <146014608+luigiazoreng@users.noreply.github.com>
2025-04-08 17:59:14 -0300

    Adds user creation with request validation and service integration
    Enhances the CreateUser endpoint to decode and validate incoming
    request data. Integrates UserService for user creation logic and
    adds appropriate error handling and logging. Responds with a
    success message upon user creation.

--- back-end/src/handler/user.go
+++ back-end/src/handler/user.go
@@ -1,11 +1,16 @@
 package handler
 
 import (
+	"encoding/json"
 	"log"
 	"net/http"
+	"observer-go/src/service"
+	"observer-go/src/structs/DTO"
+
 )
 
 type User struct {
+	UserService service.UserService
 	log *log.Logger
 }
 
@@ -14,7 +19,24 @@ func (i *User) Login(res http.ResponseWriter, req *http.Request) {
 }
 
 func (i *User) CreateUser(res http.ResponseWriter, req *http.Request) {
-	i.log.Println("CreateUser")
+	var userBody DTO.UserDTO
+	err := json.NewDecoder(req.Body).Decode(&userBody)
+	if err != nil {
+		i.log.Println("Error decoding request body:", err)
+		http.Error(res, err.Error(), http.StatusBadRequest)
+		return
+	}
+	defer req.Body.Close()
+	err = i.UserService.CreateUser(userBody)
+	if err != nil {
+		i.log.Println("Error creating user:", err)
+		http.Error(res, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	i.log.Println("User created")
+	res.WriteHeader(http.StatusCreated)
+	res.Header().Set("Content-Type", "application/json")
+	res.Write([]byte(`{"message": "User created"}`))
 }
 
 func (i *User) GetUserById(res http.ResponseWriter, req *http.Request) {
